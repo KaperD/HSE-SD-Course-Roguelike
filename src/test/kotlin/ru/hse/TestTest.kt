b6c6309af185ab5a56c923934828954896eafe45
Danil Bubnov <dkbubnov@edu.hse.ru>
2022-03-25 16:35:37 +0300

    Add GHA

---
+++ src/test/kotlin/ru/hse/TestTest.kt
@@ -0,0 +1,13 @@
+package ru.hse
+
+import main
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class TestTest {
+    @Test
+    fun test() {
+        Assertions.assertEquals(1, 1)
+        main(emptyArray())
+    }
+}
