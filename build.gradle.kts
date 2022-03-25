16a451d3d1986b2d0dc5cfe19c98f84f5fb076bc
Danil Bubnov <dkbubnov@edu.hse.ru>
2022-03-25 16:00:54 +0300

    Add detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.6.10"
     application
+    id("io.gitlab.arturbosch.detekt") version "1.19.0"
 }
 
 group = "ru.hse"
@@ -14,6 +15,7 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.19.0")
 }
 
 tasks.test {
@@ -24,6 +26,24 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "11"
 }
 
+tasks.withType<JavaCompile> {
+    sourceCompatibility = "11"
+    targetCompatibility = "11"
+}
+
 application {
     mainClass.set("MainKt")
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt> {
+    jvmTarget = "11"
+    setSource(files("src/main/kotlin"))
+    buildUponDefaultConfig = true
+    autoCorrect = true
+    config.setFrom(files("$rootDir/config/detekt/config.yml"))
+
+    reports {
+        xml.required.set(false)
+        html.required.set(true)
+    }
 }
