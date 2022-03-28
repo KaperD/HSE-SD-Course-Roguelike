7fa92029946142beaa65e1a81574ca1a18895d4f
Danil Bubnov <dkbubnov@edu.hse.ru>
2022-03-28 21:58:59 +0300

    Add property test

--- src/test/kotlin/ru/hse/roguelike/TestTest.kt
+++ src/test/kotlin/ru/hse/roguelike/TestTest.kt
@@ -2,10 +2,22 @@ package ru.hse.roguelike
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import ru.hse.roguelike.property.StringProperties
 
 class TestTest {
     @Test
     fun test() {
-        Assertions.assertEquals(1, 1)
+        Assertions.assertEquals("""
+            Open Map        = m
+            Map Free Mode   = f
+            Open Inventory  = i
+            Open Help       = h
+            Move Up         = Arrow Up
+            Move Down       = Arrow Down
+            Move Left       = Arrow Left
+            Move Right      = Arrow Right
+            Use/Cancel Item = Enter
+            Exit Game       = esc
+        """.trimIndent(), StringProperties.helpMessage)
     }
 }
