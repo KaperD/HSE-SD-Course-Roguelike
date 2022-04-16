2821d39dad71a594675eda1ae72df70d1db5cb85
ilyaonoff <ilyaonof@yandex.ru>
2022-04-16 21:28:56 +0300

    Make random strategy reproducible

--- src/main/kotlin/ru/hse/roguelike/model/creature/strategy/RandomStrategy.kt
+++ src/main/kotlin/ru/hse/roguelike/model/creature/strategy/RandomStrategy.kt
@@ -5,13 +5,14 @@ import ru.hse.roguelike.model.Position
 import ru.hse.roguelike.model.creature.mob.Mob
 import kotlin.random.Random
 
-class RandomStrategy : MoveStrategy {
+class RandomStrategy(seed: Int = 0) : MoveStrategy {
+    private val rand = Random(seed)
     override fun move(gameField: GameField, mob: Mob): Position {
         val availableNextPositions = getAvailableNextPositions(gameField, mob.position)
         if (availableNextPositions.isEmpty()) {
             return mob.position
         }
-        val chosenPosition = Random.nextInt(availableNextPositions.size)
+        val chosenPosition = rand.nextInt(availableNextPositions.size)
         return availableNextPositions[chosenPosition]
     }
 }
